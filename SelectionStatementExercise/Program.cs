f0aa2e47e2f66d790d33a81f534c313bb4921d8f
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-08-04 23:58:09 -0400

    Exercise 1 initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 5);
+            int userInput;
+            do
+            {
+                Console.WriteLine("Try to guess my favorite number!");
+                userInput = int.Parse(Console.ReadLine());
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (userInput != favNumber);
+            
+
+            
         }
     }
 }
